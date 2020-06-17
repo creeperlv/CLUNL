a88f5a70b68358a54cc3aeffa7e3f3912bc0ab0a
Creeper Lv <creeperlv38@outlook.com>
2020-06-17 20:46:03 +0800

    Made a pipeline codebase.

---
+++ CLUNL.Pipeline/IPipedProcessUnit.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CLUNL.Pipeline
+{
+    public interface IPipedProcessUnit
+    {
+        object Process(object Input);
+    }
+    public interface IPipelineProcessor : IPipedProcessUnit
+    {
+        void Init();
+    }
+    public class UniversalProcessor:IPipelineProcessor
+    {
+        List<IPipedProcessUnit> processUnits = new List<IPipedProcessUnit>();
+
+        public void Init()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var item in assemblies)
+            {
+                if (item.FullName.StartsWith("System.")) continue;
+                if (item.FullName.StartsWith("Microsoft.")) continue;
+                if (item.FullName.StartsWith("netstandard")) continue;
+                {
+                    var types=item.GetTypes();
+                    foreach (var type0 in types)
+                    {
+                        if (type0.IsAssignableFrom(typeof(IPipelineProcessor)))
+                        {
+                            continue;
+                        }
+                        if (type0.IsAssignableFrom(typeof(IPipedProcessUnit)))
+                        {
+                            processUnits.Add(Activator.CreateInstance(type0) as IPipedProcessUnit);
+                        }
+                    }
+                }
+            }
+        }
+
+        public object Process(object Input)
+        {
+
+            foreach (var item in processUnits)
+            {
+                Input = item.Process(Input);
+            }
+            return Input;
+        }
+    }
+
+    public class DefaultProcessor : IPipelineProcessor
+    {
+        List<IPipedProcessUnit> processUnits = new List<IPipedProcessUnit>();
+        public void Init(params IPipedProcessUnit[] units)
+        {
+            if (units.Length == 0)
+            {
+                processUnits.Add(new DefaultProcessUnit());
+            }
+        }
+        public void Init()
+        {
+            Init(new IPipedProcessUnit[0]);
+        }
+
+        public object Process(object Input)
+        {
+
+            foreach (var item in processUnits)
+            {
+                Input = item.Process(Input);
+            }
+            return Input;
+        }
+    }
+    public class DefaultProcessUnit : IPipedProcessUnit
+    {
+        public object Process(object Input)
+        {
+            return Input;
+        }
+    }
+}
---
+++ CLUNLTests/PipelineTest.cs
@@ -0,0 +1,27 @@
+﻿using CLUNL.Pipeline;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNLTests
+{
+    [TestClass]
+    public class PipelineTest
+    {
+        [TestMethod]
+        public void PipeLineTest0()
+        {
+            DefaultProcessor defaultProcessor = new DefaultProcessor();
+            defaultProcessor.Init();
+            var assemblies= AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var item in assemblies)
+            {
+                if (item.FullName.StartsWith("System.")) continue;
+                if (item.FullName.StartsWith("Microsoft.")) continue;
+                if (item.FullName.StartsWith("netstandard")) continue;
+                Console.WriteLine(defaultProcessor.Process(item.FullName));
+            }
+        }
+    }
+}
