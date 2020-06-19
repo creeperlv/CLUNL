2301359bb76779c6b1b60fcc294227e1f67f0aa8
Creeper Lv <creeperlv38@outlook.com>
2020-06-20 00:43:13 +0800

    Working on packaging...

--- CLUNL.Packaging/MultiPackagePack.cs
+++ CLUNL.Packaging/MultiPackagePack.cs
@@ -7,13 +7,17 @@ namespace CLUNL.Packaging
 {
     public class MultiPackagePack
     {
-        public static void CreatePack(Dictionary<string,string>Manifest,DirectoryInfo TempDirectory, FileInfo TargetPackage)
+        public static void CreatePack(Dictionary<string,DirectoryInfo>Manifest,DirectoryInfo TempDirectory, FileInfo TargetPackage)
         {
             FileInfo fileInfo = new FileInfo(Path.Combine(TempDirectory.FullName, "Package.Manifest"));
+            StreamWriter sw = new StreamWriter(fileInfo.OpenWrite());
             foreach (var item in Manifest)
             {
-
+                sw.WriteLine(item.Key + "|" + item.Value.Name);
             }
+
+            sw.Close();
+
         }
         public static void ExtractPack(params string[] PackageName)
         {
---
+++ Creeper Lv's Universal dotNet Library/Data/Layer0/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNL.Data.Layer0
+{
+    public static class Utilities
+    {
+        public static void CopyFolderRecursive(string Origin,string Target)
+        {
+
+        }
+    }
+}
