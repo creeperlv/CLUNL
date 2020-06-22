7e3c28979aee6576637c04f089baa6d9597ab6a4
Creeper Lv <creeperlv38@outlook.com>
2020-06-22 23:02:54 +0800

    Working on serialization.

---
+++ Creeper Lv's Universal dotNet Library/Data/Layer2/ObjectData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace CLUNL.Data.Layer2
+{
+    public class ObjectData
+    {
+        public static void BinSerialize(Object obj,Stream Target) {
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(Target, obj);
+        }
+        public static object BinDeserialize(Stream Origin)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(Origin);
+        }
+        public static void XmlSerialize(Object obj,Stream Target)
+        {
+
+        }
+    }
+}
