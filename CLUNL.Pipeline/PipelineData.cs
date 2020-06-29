d9d314cc0ea7c7b7d8989c6479deacf051324021
Creeper Lv <creeperlv38@outlook.com>
2020-06-30 00:26:15 +0800

    Improved CLUNL.Pipeline. -It won't verify continuity through Options, instead, through a given ID.

--- CLUNL.Pipeline/PipelineData.cs
+++ CLUNL.Pipeline/PipelineData.cs
@@ -4,26 +4,38 @@ using System.Text;
 
 namespace CLUNL.Pipeline
 {
+    [Serializable]
     public class PipelineData
     {
+        static Random RandomID = new Random();
         public object PrimaryData;
         public object SecondaryData;
         public object Options { get; }
-        public PipelineData(object PrimaryData,object SecondaryData,object Options)
+        public int DataID { get; } = -1;
+        public PipelineData(object PrimaryData, object SecondaryData, object Options, int DataID = -1)
         {
             this.PrimaryData = PrimaryData;
             this.SecondaryData = SecondaryData;
             if (Options == null) this.Options = new object();
             else
-            this.Options = Options;
+                this.Options = Options;
+            if (DataID == -1)
+            {
+                if (LibraryInfo.GetFlag(FeatureFlags.Pipeline_AutoID_Random) == 1)
+                {
+                    this.DataID = RandomID.Next();
+                }
+                else
+                    this.DataID = Options.GetHashCode();
+            }
         }
         public bool CheckContinuity(PipelineData Target)
         {
             return CheckContinuity(this, Target);
         }
-        public static bool CheckContinuity(PipelineData Original,PipelineData New)
+        public static bool CheckContinuity(PipelineData Original, PipelineData New)
         {
-            return Original.Options.Equals(New.Options);
+            return Original.DataID.Equals(New.DataID);
         }
     }
 }
--- Creeper Lv's Universal dotNet Library/LibraryInfo.cs
+++ Creeper Lv's Universal dotNet Library/LibraryInfo.cs
@@ -29,6 +29,7 @@ namespace CLUNL
         public static int ThrowExceptionWhenHold = 0x0002;
         public static int FileWR_AutoCreateFile = 0x0003;
         public static int Pipeline_IgnoreError=0x0004;
+        public static int Pipeline_AutoID_Random=0x0005;
     }
     public enum VersionChannel
     {
