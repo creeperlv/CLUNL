b6c65cf01465190b7c3bd3782adb55dba0488cfc
Creeper Lv <creeperlv38@outlook.com>
2020-06-30 17:03:10 +0800

    Added a processor that can find process units according to a string list.

--- CLUNL.Pipeline/Processors.cs
+++ CLUNL.Pipeline/Processors.cs
@@ -3,7 +3,88 @@ using System.Collections.Generic;
 
 namespace CLUNL.Pipeline
 {
+    public class NameSpecifiedProcessor : IPipelineProcessor
+    {
+        List<IPipedProcessUnit> processUnits = new List<IPipedProcessUnit>();
 
+        public void Init()
+        {
+
+            throw new Exception("Please specify process units.");
+        }
+
+
+        public void Init(ProcessUnitManifest manifest)
+        {
+            processUnits = manifest.GetUnitInstances();
+        }
+        public void Init(List<String> manifest)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var item in assemblies)
+            {
+                if (item.FullName.StartsWith("System.")) continue;
+                if (item.FullName.StartsWith("Microsoft.")) continue;
+                if (item.FullName.StartsWith("netstandard")) continue;
+                {
+                    foreach (var processUnit in manifest)
+                    {
+                        try
+                        {
+                            processUnits.Add((IPipedProcessUnit)item.CreateInstance(processUnit));
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+            }
+        }
+        public PipelineData Process(PipelineData Input)
+        {
+            return Process(Input, false);
+        }
+
+        public PipelineData Process(PipelineData Input, bool IgnoreError)
+        {
+            if (IgnoreError)
+            {
+
+                foreach (var item in processUnits)
+                {
+                    try
+                    {
+                        var output = item.Process(Input);
+                        if (Input.CheckContinuity(output))
+                        {
+                            Input = output;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                return Input;
+            }
+            else
+            {
+
+                foreach (var item in processUnits)
+                {
+                    var output = item.Process(Input);
+                    if (Input.CheckContinuity(output))
+                    {
+                        Input = output;
+                    }
+                    else
+                    {
+                        throw new PipelineDataContinuityException(item);
+                    }
+                }
+                return Input;
+            }
+        }
+    }
     public class UniversalProcessor : IPipelineProcessor
     {
         List<IPipedProcessUnit> processUnits = new List<IPipedProcessUnit>();
