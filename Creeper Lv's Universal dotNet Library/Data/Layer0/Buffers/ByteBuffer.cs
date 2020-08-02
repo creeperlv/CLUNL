e73ca5a30003d95544c986121976787789183c95
Creeper Lv <creeperlv38@outlook.com>
2020-08-03 03:10:36 +0800

    Added a buffer that can log byte[] and supposed to be effective without using native code.

---
+++ Creeper Lv's Universal dotNet Library/Data/Layer0/Buffers/ByteBuffer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNL.Data.Layer0.Buffers
+{
+    public class ByteBuffer:IEnumerable<byte[]>
+    {
+        Queue<byte> buf = new Queue<byte>();
+        public void AppendGroup(byte[] data)
+        {
+            var H = BitConverter.GetBytes(data.Length);
+            foreach (var item in H)
+            {
+                buf.Enqueue(item);
+            }
+            foreach (var item in data)
+            {
+                buf.Enqueue(item);
+            }
+        }
+
+        public IEnumerator<byte[]> GetEnumerator()
+        {
+            while (buf.Count<1)
+            {
+                yield return GetGroup();
+            }
+        }
+        public byte[] GetTotalData()
+        {
+            return buf.ToArray();
+        }
+        public byte[] GetGroup()
+        {
+            byte[] Header = new byte[4];
+            Header[0] = buf.Dequeue();
+            Header[1] = buf.Dequeue();
+            Header[2] = buf.Dequeue();
+            Header[3] = buf.Dequeue();
+            var TargetLength = BitConverter.ToInt32(Header, 0);
+            byte[] RealContent = new byte[TargetLength];
+            for (int i = 0; i < TargetLength; i++)
+            {
+                RealContent[i] = buf.Dequeue();
+            }
+            return RealContent;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            while (buf.Count < 1)
+            {
+                yield return GetGroup();
+            }
+        }
+    }
+}
