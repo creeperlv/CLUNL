3253939b1722ea65c97d4c2dc78dba661fb2a415
Creeper Lv <creeperlv38@outlook.com>
2020-09-14 11:13:06 +0800

    Added an interface of data convertor. Added a manager of convertors.

---
+++ Creeper Lv's Universal dotNet Library/Data/Convertors/ConvertorManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNL.Data.Convertors
+{
+    public class ConvertorManager
+    {
+        public static ConvertorManager CurrentConvertorManager = new ConvertorManager();
+        Dictionary<Type, IConvertor> Convertors = new Dictionary<Type, IConvertor>();
+        ConvertorManager()
+        {
+
+        }
+        public void RegisterConvertor(Type T,IConvertor Convertor)
+        {
+            if (Convertors.ContainsKey(T)) Convertors[T] = Convertor; else Convertors.Add(T, Convertor);
+        }
+        public void UnregisterConvertor(Type T)
+        {
+            Convertors.Remove(T);
+        }
+        public IConvertor GetConvertor(Type T)
+        {
+            return Convertors[T];
+        }
+    }
+}
---
+++ Creeper Lv's Universal dotNet Library/Data/Convertors/IConvertor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNL.Data.Convertors
+{
+    public interface IConvertor
+    {
+        byte[] Serialize();
+        object Deserialize(byte[] b);
+    }
+}
