17238e1a47635653496d4b3f8b5cab6a00a4b641
Creeper Lv <creeperlv38@outlook.com>
2020-09-14 17:59:56 +0800

    Added 3 interfaces to present abstract concept of some types that can be convert to bytes.

---
+++ Creeper Lv's Universal dotNet Library/Data/Layer0/Buffers/IByteBufferable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLUNL.Data.Layer0.Buffers
+{
+    public interface IByteBufferable
+    {
+        void Deserialize(ByteBuffer buffer);
+        ByteBuffer Serialize();
+    }
+    public interface IDataBufferable
+    {
+        void Deserialize(DataBuffer buffer);
+        DataBuffer Serialize();
+    }
+    public interface ITypeDataBufferable
+    {
+        void Deserialize(TypeDataBuffer buffer);
+        TypeDataBuffer Serialize();
+    }
+}
