fd76deaebbccf73f4603f4937213a9f805710414
Creeper Lv <creeperlv38@outlook.com>
2020-09-29 14:33:49 +0800

    Creating Diagnosis Tools.

---
+++ CLUNL.Diagnosis/DebuggerClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CLUNL.Diagnosis
+{
+    public class DebuggerClient
+    {
+        DebuggerClientProfile profile;
+        public DebuggerClientProfile Profile { get=>profile;  }
+        public static DebuggerClient CurrentClient;
+        public static void Init(DebuggerClientProfile clientProfile,string Path)
+        {
+            CurrentClient = new DebuggerClient();
+            CurrentClient.profile = clientProfile;
+            CurrentClient.Init(Path);
+        }
+        public void Init(string Path)
+        {
+            switch (profile)
+            {
+                case DebuggerClientProfile.LocalFile:
+
+                    break;
+                case DebuggerClientProfile.MappedFile:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public enum DebuggerClientProfile
+    {
+        LocalFile,MappedFile
+    }
+}
