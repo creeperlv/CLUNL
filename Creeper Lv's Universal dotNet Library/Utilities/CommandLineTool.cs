abeb31446522aba58bb118b6edc17bcd4f70fedb
Creeper Lv <creeperlv38@outlook.com>
2021-04-27 19:16:28 +0800

    Working on command line string analyzer.

--- Creeper Lv's Universal dotNet Library/Utilities/CommandLineTool.cs
+++ Creeper Lv's Universal dotNet Library/Utilities/CommandLineTool.cs
@@ -5,10 +5,54 @@ using System.Text;
 namespace CLUNL.Utilities
 {
     /// <summary>
+    /// CommandLine class, contains the analyzed results.
+    /// </summary>
+    public class CommandLine
+    {
+        /// <summary>
+        /// Real Parameters.
+        /// </summary>
+        public List<string> RealParameter = new List<string>();
+        /// <summary>
+        /// Parameters those starts with '-'
+        /// </summary>
+        public List<string> PresentSwitches = new List<string>();
+        /// <summary>
+        /// Judge whether a switch with given key is on.(Case Insensitive)
+        /// e.g.: "-Foo" means "foo" is on, and the query of "foo" returns true.
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        public bool IsOn(string Key)
+        {
+            if (PresentSwitches.Contains(Key.ToUpper()))return true;
+            return false;
+        }
+    }
+    /// <summary>
     /// Tool to help deal with command-line.
     /// </summary>
     public class CommandLineTool
     {
+        /// <summary>
+        /// Analyze a line of string into CommandLine.
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        public static CommandLine Analyze(string cmd)
+        {
+            CommandLine commandLine = new CommandLine();
+            var parameter = CommandParse(cmd);
+            commandLine.RealParameter = parameter;
+            foreach (var item in parameter)
+            {
+                if (item.StartsWith("-"))
+                {
+                    commandLine.PresentSwitches.Add(item.Substring(1).ToUpper());
+                }
+            }
+            return commandLine;
+        }
         /// <summary>
         /// Parse a command line into a string list.
         /// </summary>
