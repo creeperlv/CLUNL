9d1f152b938ae432b89f73952958511013e1f7fe
Creeper Lv <creeperlv38@outlook.com>
2021-07-17 01:25:26 +0800

    Working on Checkpoint System...

---
+++ CLUNLTests/CheckPointSystemTest.cs
@@ -0,0 +1,89 @@
+﻿using CLUNL.Data.Serializables.CheckpointSystem;
+using CLUNL.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLUNLTests
+{
+    [TestClass]
+    public class CheckPointSystemTest
+    {
+        [TestMethod]
+        public void SaveAndLoadTest()
+        {
+            CheckpointSystem.Init((new DirectoryInfo(".").FullName));
+
+            var CP=CheckpointSystem.CurrentCheckpointSystem.GetOrCreateCheckPoint("TestCP");
+            List<Data00> OriginalData = new List<Data00>();
+            List<Data00> CPData = new List<Data00>();
+            for (int i = 0; i < 100; i++)
+            {
+                var str = RandomTool.GetRandomString(10, RandomStringRange.R3);
+                {
+                    Data00 data = new Data00();
+                    data.Name = $"D{i}";
+                    data.Field00 = i % 10;
+                    data.Field01 = i * 10;
+                    data.Field02 = str;
+                    CP.RegisterCheckPointData(data);
+                    CPData.Add(data);
+                }
+                {
+                    Data00 data = new Data00();
+                    data.Name = $"D{i}";
+                    data.Field00 = i % 10;
+                    data.Field01 = i * 10;
+                    data.Field02 = str;
+                    OriginalData.Add(data);
+                }
+
+            }
+            CP.LoadSnapshot(CP.CreateSnapshot());
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(CPData[i], OriginalData[i]);
+            }
+        }
+    }
+    public class Data00 : ICheckpointData
+    {
+        public string Name;
+        public string GetName() => Name;
+        public int Field00;
+        public int Field01;
+        public string Field02;
+        public void Load(List<object> data)
+        {
+            Trace.WriteLine(data[0]+":"+data[0].GetType());
+            Trace.WriteLine(data[1] + ":" + data[1].GetType());
+            Trace.WriteLine(data[2] + ":" + data[2].GetType());
+            Field00 = (int)data[0];
+            Field01 = (int)data[1];
+            Field02 = (string)data[2];
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj is Data00 d)
+            {
+                return Field00 == d.Field00 && Field01 == d.Field01 && Field02 == d.Field02;
+            }
+            return base.Equals(obj);
+        }
+
+        public List<object> Save()
+        {
+            return new List<object> { Field00, Field01, Field02 };
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
--- Creeper Lv's Universal dotNet Library/Data/Serializables/CheckpointSystem/CheckPoint.cs
+++ Creeper Lv's Universal dotNet Library/Data/Serializables/CheckpointSystem/CheckPoint.cs
@@ -15,7 +15,10 @@ namespace CLUNL.Data.Serializables.CheckpointSystem
         DirectoryInfo StorageFolder;
         static JsonSerializerSettings serializerSettings = new JsonSerializerSettings
         {
-            TypeNameHandling = TypeNameHandling.All
+            TypeNameHandling = TypeNameHandling.All,
+            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+            NullValueHandling = NullValueHandling.Ignore,
+            PreserveReferencesHandling = PreserveReferencesHandling.All,
         };
         internal CheckPoint(DirectoryInfo di)
         {
@@ -38,15 +41,22 @@ namespace CLUNL.Data.Serializables.CheckpointSystem
         /// <summary>
         /// Create a snapshot of current state.
         /// </summary>
-        public void CreateSnapshot()
+        public string CreateSnapshot()
         {
-            Dictionary<string, List<object>> RealData = new Dictionary<string, List<object>>();
+            Dictionary<string, object> RealData = new Dictionary<string, object>();
             foreach (var item in ReferenceDatas)
             {
-                RealData.Add(item.Key, item.Value.Save());
+                var d = item.Value.Save();
+                RealData.Add($"{item.Key}.Count", d.Count);
+                for (int i = 0; i < d.Count; i++)
+                {
+
+                    RealData.Add($"{item.Key}.{i}", d[i]);
+                }
             }
             var Name = Guid.NewGuid().ToString();
             File.WriteAllText(Path.Combine(StorageFolder.FullName, Name), JsonConvert.SerializeObject(RealData, serializerSettings));
+            return Name;
         }
         /// <summary>
         /// Load a snapshot with given name of the checkpoint.
@@ -57,12 +67,26 @@ namespace CLUNL.Data.Serializables.CheckpointSystem
             if (File.Exists(path))
             {
 
-                Dictionary<string, List<object>> RealData;
-                RealData = JsonConvert.DeserializeObject<Dictionary<string, List<object>>>(File.ReadAllText(path), serializerSettings);
-                foreach (var item in RealData)
+                Dictionary<string, object> RealData;
+                RealData = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(path), serializerSettings);
+                foreach (var item in ReferenceDatas)
                 {
-                    ReferenceDatas[item.Key].Load(item.Value);
+                    if (RealData.ContainsKey($"{item.Key}.Count"))
+                    {
+                        int Count = (int)RealData[$"{item.Key}.Count"];
+                        List<Object> d = new List<object>();
+                        for (int i = 0; i < Count; i++)
+                        {
+                            d.Add(RealData[$"{item.Key}.{i}"]);
+                        }
+                        ReferenceDatas[item.Key].Load(d);
+                    }
                 }
+
+                //foreach (var item in RealData)
+                //{
+                //    ReferenceDatas[item.Key].Load(item.Value);
+                //}
 
             }
         }
