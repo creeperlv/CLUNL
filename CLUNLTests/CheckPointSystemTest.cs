1d13a5c6ac5af3ee32a221e0587f22c374d4b361
Creeper Lv <creeperlv38@outlook.com>
2021-07-18 00:03:33 +0800

    Updated version to 0.0.3.1 -Added `FloatNumber`.

--- CLUNLTests/CheckPointSystemTest.cs
+++ CLUNLTests/CheckPointSystemTest.cs
@@ -20,7 +20,7 @@ namespace CLUNLTests
         {
             CheckpointSystem.Init((new DirectoryInfo(".").FullName));
 
-            var CP=CheckpointSystem.CurrentCheckpointSystem.GetOrCreateCheckPoint("TestCP");
+            var CP = CheckpointSystem.CurrentCheckpointSystem.GetOrCreateCheckPoint("TestCP");
             List<Data00> OriginalData = new List<Data00>();
             List<Data00> CPData = new List<Data00>();
             for (int i = 0; i < 100; i++)
@@ -32,6 +32,8 @@ namespace CLUNLTests
                     data.Field00 = i % 10;
                     data.Field01 = i * 10;
                     data.Field02 = str;
+                    data.Field03 = i / 10f;
+                    data.Field04 = i / 10.0;
                     CP.RegisterCheckPointData(data);
                     CPData.Add(data);
                 }
@@ -41,6 +43,8 @@ namespace CLUNLTests
                     data.Field00 = i % 10;
                     data.Field01 = i * 10;
                     data.Field02 = str;
+                    data.Field03 = i / 10f;
+                    data.Field04 = i / 10.0;
                     OriginalData.Add(data);
                 }
 
@@ -59,24 +63,28 @@ namespace CLUNLTests
         public IntNumber Field00;
         public IntNumber Field01;
         public string Field02;
+        public FloatNumber Field03;
+        public double Field04;
         public void Load(List<object> data)
         {
             Field00 = (IntNumber)data[0];
             Field01 = (IntNumber)data[1];
             Field02 = (string)data[2];
+            Field03 = (FloatNumber)data[3];
+            Field04 = (double)data[4];
         }
         public override bool Equals(object obj)
         {
-            if(obj is Data00 d)
+            if (obj is Data00 d)
             {
-                return Field00 == d.Field00 && Field01 == d.Field01 && Field02 == d.Field02;
+                return Field00 == d.Field00 && Field01 == d.Field01 && Field02 == d.Field02 && Field03 == d.Field03 && Field04 == d.Field04;
             }
             return base.Equals(obj);
         }
 
         public List<object> Save()
         {
-            return new List<object> { Field00, Field01, Field02 };
+            return new List<object> { Field00, Field01, Field02, Field03, Field04 };
         }
 
         public override int GetHashCode()
--- Creeper Lv's Universal dotNet Library/Data/Serializables/CheckpointSystem/Types/Number.cs
+++ Creeper Lv's Universal dotNet Library/Data/Serializables/CheckpointSystem/Types/Number.cs
@@ -23,13 +23,6 @@ namespace CLUNL.Data.Serializables.CheckpointSystem.Types
         }
 
         /// <summary>
-        /// Int Number, helpful when using Checkpoint.
-        /// </summary>
-        public IntNumber()
-        {
-
-        }
-        /// <summary>
         /// Convert from long to IntNumber;
         /// </summary>
         /// <param name="value"></param>
@@ -105,6 +98,118 @@ namespace CLUNL.Data.Serializables.CheckpointSystem.Types
                 return I == Data;
             }
             return base.Equals(obj);
+        }
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    /// <summary>
+    /// Float Number, helpful when using Checkpoint.
+    /// </summary>
+    public class FloatNumber
+    {
+        /// <summary>
+        /// Real data.
+        /// </summary>
+        public float Data;
+
+        /// <summary>
+        /// Float Number, helpful when using Checkpoint.
+        /// </summary>
+        public FloatNumber(float data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Convert from double to FloatNumber;
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator FloatNumber(double value)
+        {
+            return new FloatNumber((float)value);
+        }
+        /// <summary>
+        /// Convert from float to FloatNumber;
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator FloatNumber(float value)
+        {
+            return new FloatNumber(value);
+        }
+        /// <summary>
+        /// To float.
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator float(FloatNumber value)
+        {
+            return value.Data;
+        }
+        /// <summary>
+        /// To double
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator double(FloatNumber value)
+        {
+            return (double)value.Data;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="L"></param>
+        /// <param name="R"></param>
+        /// <returns></returns>
+        public static bool operator ==(FloatNumber L, FloatNumber R)
+        {
+            return L.Data == R.Data;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="L"></param>
+        /// <param name="R"></param>
+        /// <returns></returns>
+        public static bool operator !=(FloatNumber L, FloatNumber R)
+        {
+            return L.Data != R.Data;
+        }
+        /// <summary>
+        /// Converts the numeric value of this instance to its equivalent string representation.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is FloatNumber N)
+            {
+                return N.Data == Data;
+            }
+            else if (obj is int I)
+            {
+                return I == Data;
+            }
+            return base.Equals(obj);
+        }
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
