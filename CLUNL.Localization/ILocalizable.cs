d2988f2e8f3e6a5d0da273bd054b18d43fea9cf1
Creeper Lv <creeperlv@users.noreply.github.com>
2022-01-28 23:11:12 +0800

    CLUNL.Localization 0.0.6.0 Added ILocalizable and LocalizedString to help build a localized application.

---
+++ CLUNL.Localization/ILocalizable.cs
@@ -0,0 +1,13 @@
+﻿namespace CLUNL.Localization
+{
+    /// <summary>
+    /// Mark a class is localizable
+    /// </summary>
+    public interface ILocalizable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void ApplyLocalization();
+    }
+}
---
+++ CLUNL.Localization/LocalizedString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CLUNL.Localization
+{
+    /// <summary>
+    /// Localized via Language.
+    /// </summary>
+    public class LocalizedString
+    {
+        /// <summary>
+        /// Initialize the string with LanguageID and fallback.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="Fallback"></param>
+        public LocalizedString(string ID,string Fallback)
+        {
+            this.ID = ID;
+            this.Fallback = Fallback;
+        }
+        private string ID;
+        private string Fallback;
+        /// <summary>
+        /// Return the localized string.
+        /// </summary>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override string ToString()
+        {
+            return Language.Find(ID, Fallback);
+        }
+        /// <summary>
+        /// Return the value from ToString();
+        /// </summary>
+        /// <param name="L"></param>
+        public static implicit operator string(LocalizedString L)
+        {
+            return L.ToString();
+        }
+    }
+}
