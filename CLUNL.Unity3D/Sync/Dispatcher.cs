31ee03f4c651b8b2f32e267875ede231691edea5
Creeper Lv <creeperlv@users.noreply.github.com>
2022-02-18 18:22:17 +0800

    Added a prototype Dispatcher that aims on an equivalent of which in WPF/UWP/AvaloniaUI.

---
+++ CLUNL.Unity3D/Sync/Dispatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+//using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CLUNL.Unity3D.Sync
+{
+    public class Dispatcher
+    {
+        //static ImmutableQueue<Action> iactions = ImmutableQueue<Action>.Empty;
+        static ConcurrentQueue<Action> actions = new ConcurrentQueue<Action>();
+        static object _lock = new object();
+        /// <summary>
+        /// Call to perform a sync operation every frame.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Sync()
+        {
+            lock (_lock)
+            {
+                while (actions.Count > 0)
+                {
+                    if (actions.TryDequeue(out Action action))
+                    {
+                        action();
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Enqueue an action from non-main thread.
+        /// </summary>
+        /// <param name="action"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Invoke(Action action)
+        {
+            lock (_lock)
+            {
+                actions.Enqueue(action);
+            }
+        }
+    }
+}
---
+++ CLUNL.Unity3D/Sync/DispatcherController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CLUNL.Unity3D.Sync
+{
+    /// <summary>
+    /// Default of a dispatcher controller.
+    /// </summary>
+    public class DispatcherController : MonoBehaviour
+    {
+        static bool __started = false;
+        public void Start()
+        {
+            if (__started)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+        public void Update()
+        {
+            Dispatcher.Sync();
+        }
+    }
+}
