1bbe8c40e56ba40685eaf6628e6002c2e1f60790
Creeper Lv <creeperlv@users.noreply.github.com>
2022-02-27 18:21:24 +0800

    Improving understandability.

--- Creeper Lv's Universal dotNet Library/Utilities/Argument.cs
+++ Creeper Lv's Universal dotNet Library/Utilities/Argument.cs
@@ -23,98 +23,17 @@ namespace CLUNL.Utilities
             else
             if (EntireArgument[0] == '-' && (EntireArgument.IndexOf(":") > 0 || EntireArgument.IndexOf("=") > 0))
             {
-
                 List<string> vs = new List<string>();
                 StringBuilder stringBuilder = new StringBuilder();
                 StringBuilder stringBuilder2 = new StringBuilder();
                 bool a = false;
                 bool b = false;
-                int l = EntireArgument.Length;
-                char c;
                 bool isKeyLocated = false;
-                for (int i = 1; i < l; i++)
-                {
-                    c = EntireArgument[i];
-                    if (isKeyLocated)
-                        stringBuilder2.Append(c);
-                    if (b == true)
-                    {
-                        switch (c)
-                        {
-                            case '\\':
-                                stringBuilder.Append('\\');
-                                break;
-                            case ';':
-                                stringBuilder.Append(';');
-                                break;
-                            case ':':
-                                stringBuilder.Append(':');
-                                break;
-                            case '=':
-                                stringBuilder.Append('=');
-                                break;
-                            default:
-                                break;
-                        }
-                        b = false;
-                        continue;
-                    }
-                    else
-                    {
-                        if (c == '\\')
-                        {
-                            b = true;
-                            continue;
-                        }
-                        if (c == ';')
-                        {
-                            if (a == false)
-                            {
-                                if (stringBuilder.Length > 0)
-                                {
-                                    vs.Add((stringBuilder.ToString()));
-                                    stringBuilder.Clear();
-
-                                }
-                                continue;
-                            }
-                        }
-                        if (isKeyLocated == false)
-                        {
-                            switch (c)
-                            {
-                                case ':':
-                                case '=':
-                                    if (a == false)
-                                    {
-                                        isKeyLocated = true;
-
-                                        CollectionNameStrictCase = stringBuilder.ToString();
-                                        CollectionName = CollectionNameStrictCase.ToUpper();
-                                        stringBuilder.Clear();
-                                        continue;
-                                    }
-
-                                    break;
-                            }
-                        }
-                        if (c == '|')
-                        {
-                            if (a == false)
-                            {
-                                vs.Add((stringBuilder.ToString()));
-                                stringBuilder.Clear();
-                                break;
-                            }
-                        }
-                    }
-                    stringBuilder.Append(c);
-                }
+                ApplyResolve0(vs, stringBuilder, stringBuilder2, a, ref b, ref isKeyLocated);
                 if (stringBuilder.Length > 0)
                 {
-                    vs.Add((stringBuilder.ToString()));
+                    vs.Add(stringBuilder.ToString());
                     stringBuilder.Clear();
-
                 }
                 if (isKeyLocated == true)
                 {
@@ -126,6 +45,102 @@ namespace CLUNL.Utilities
                 stringBuilder.Clear();
             }
         }
+
+        private void ApplyResolve0(List<string> vs, StringBuilder stringBuilder, StringBuilder stringBuilder2, bool a, ref bool b, ref bool isKeyLocated)
+        {
+            int l = EntireArgument.Length;
+            char c;
+            for (int i = 1; i < l; i++)
+            {
+                c = EntireArgument[i];
+                if (isKeyLocated)
+                    stringBuilder2.Append(c);
+                if (b == true)
+                {
+                    b = Resolve0_1(stringBuilder, c);
+                    continue;
+                }
+                else
+                {
+                    if (Resolve0_2(vs, stringBuilder, a, ref b, ref isKeyLocated, c)) break;
+                }
+                stringBuilder.Append(c);
+            }
+        }
+
+        private bool Resolve0_2(List<string> vs, StringBuilder stringBuilder, bool a, ref bool b, ref bool isKeyLocated, char c)
+        {
+            if (c == '\\')
+            {
+                b = true;
+                return false;
+            }
+            if (c == ';')
+            {
+                if (a == false)
+                {
+                    if (stringBuilder.Length > 0)
+                    {
+                        vs.Add((stringBuilder.ToString()));
+                        stringBuilder.Clear();
+                    }
+                    return false;
+                }
+            }
+            if (isKeyLocated == false)
+            {
+                switch (c)
+                {
+                    case ':':
+                    case '=':
+                        if (a == false)
+                        {
+                            isKeyLocated = true;
+
+                            CollectionNameStrictCase = stringBuilder.ToString();
+                            CollectionName = CollectionNameStrictCase.ToUpper();
+                            stringBuilder.Clear();
+                            return false;
+                        }
+                        break;
+                }
+            }
+            if (c == '|')
+            {
+                if (a == false)
+                {
+                    vs.Add((stringBuilder.ToString()));
+                    stringBuilder.Clear();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool Resolve0_1(StringBuilder stringBuilder, char c)
+        {
+            bool b;
+            switch (c)
+            {
+                case '\\':
+                    stringBuilder.Append('\\');
+                    break;
+                case ';':
+                    stringBuilder.Append(';');
+                    break;
+                case ':':
+                    stringBuilder.Append(':');
+                    break;
+                case '=':
+                    stringBuilder.Append('=');
+                    break;
+                default:
+                    break;
+            }
+            b = false;
+            return b;
+        }
+
         /// <summary>
         /// Indicates if the argument is a switch.
         /// </summary>
